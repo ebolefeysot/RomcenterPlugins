de41a22a6cd0e57795043f786917f3fa810b3114
Eric Bole-Feysot <help@romcenter.com>
2024-02-07 19:25:40 +0100

    Update version and doc

--- a7800/SegaGenesis/Properties/AssemblyInfo.cs
+++ a7800/SegaGenesis/Properties/AssemblyInfo.cs
@@ -1,14 +1,13 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SegaMegadrive")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Sega Megadrive/Genesis/32x romcenter signature plugin")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Romcenter")]
 [assembly: AssemblyProduct("SegaMegadrive")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("5.0.0.0")]
+[assembly: AssemblyFileVersion("5.0.0.0")]
--- a7800/gbx/Properties/AssemblyInfo.cs
+++ a7800/gbx/Properties/AssemblyInfo.cs
@@ -1,15 +1,14 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("gbx")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("gameboy")]
+[assembly: AssemblyDescription("Gameboy romcenter signature plugin")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("gbx")]
+[assembly: AssemblyCompany("Romcenter")]
+[assembly: AssemblyProduct("gameboy")]
 [assembly: AssemblyCopyright("Copyright ©  2024")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
